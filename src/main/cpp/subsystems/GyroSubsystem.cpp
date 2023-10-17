fdddf70a204e76180a3f0c1a7db55f595408d297
ACERobotics <>
2023-10-17 16:43:05 -0700

    Implemented GryoSubsystem and LimeLightSubsystem

---
+++ src/main/cpp/subsystems/GyroSubsystem.cpp
@@ -0,0 +1,19 @@
+#include "subsystems/GyroSubsystem.h"
+
+#include <iostream>
+
+GyroSubsystem::GyroSubsystem() = default;
+
+void GyroSubsystem::zeroGyro(){
+    m_Gyro.Reset();
+}
+
+units::degree_t GyroSubsystem::getAngleDegrees() {
+    return units::degree_t{m_Gyro.GetAngle()};
+}
+
+double GyroSubsystem::getPitch() {
+    return m_Gyro.GetPitch();
+}
+
+void GyroSubsystem::Periodic() {}
--- src/main/cpp/subsystems/SwerveDriveSubsystem.cpp
+++ src/main/cpp/subsystems/SwerveDriveSubsystem.cpp
@@ -2,7 +2,11 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-#include "subsystems/ExampleSubsystem.h"
+#include "subsystems/SwerveDriveSubsystem.h"
+
+#include <frc/shuffleboard/Shuffleboard.h>
+#include <frc/shuffleboard/ShuffleboardLayout.h>
+#include <frc/shuffleboard/ShuffleboardTab.h>
 
 ExampleSubsystem::ExampleSubsystem() {
   // Implementation of subsystem constructor goes here.
